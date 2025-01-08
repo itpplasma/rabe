5b6c58d8afb9fbf43dd9dc13ec172e7cfb41829d
GeorgGrassler <georg.grassler@tugraz.at>
2025-01-08 19:00:28 +0100

    indent

--- src/main.f90
+++ src/main.f90
@@ -1,22 +1,22 @@
-program redl
-    use, intrinsic :: iso_fortran_env, only: dp => real64
-    use read_file, only: read_field_file, read_boozer_file
+program rabe
+use, intrinsic :: iso_fortran_env, only: dp => real64
+use read_file, only: read_field_file, read_boozer_file
 
-    call printer(read_boozer_file, "test.bc")
+call printer(read_boozer_file, "test.bc")
 
-    contains
+contains
 
-        subroutine printer(reader, field_file)
-            procedure(read_field_file)  :: reader
-            character(len=*) :: field_file
+subroutine printer(reader, field_file)
+    procedure(read_field_file)  :: reader
+    character(len=*) :: field_file
 
-            character(len=20) :: message
-            real(dp) :: B 
+    character(len=20) :: message
+    real(dp) :: B 
 
-            message = "Starting rabe!"
-            print *, message
-            call reader(field_file, B)
-            print *, B
-        end subroutine printer
+    message = "Starting rabe!"
+    print *, message
+    call reader(field_file, B)
+    print *, B
+end subroutine printer
 
-end program redl
+end program rabe
--- src/read_file.f90
+++ src/read_file.f90
@@ -2,27 +2,27 @@ module read_file
 use, intrinsic :: iso_fortran_env, only: dp => real64
 
 
-    abstract interface
-        subroutine read_field_file(file_name, B)
-            use, intrinsic :: iso_fortran_env, only: dp => real64
-            character(len=256), intent(in) :: file_name
-            real(dp), intent(out) :: B 
-        end subroutine read_field_file
-    end interface
+abstract interface
+subroutine read_field_file(file_name, B)
+    use, intrinsic :: iso_fortran_env, only: dp => real64
+    character(len=256), intent(in) :: file_name
+    real(dp), intent(out) :: B 
+end subroutine read_field_file
+end interface
 
 
-    contains
+contains
 
 
-        subroutine read_boozer_file(file_name, B)
-            character(len=256), intent(in) :: file_name
-            real(dp), intent(out) :: B 
-            
-            integer :: file_unit
+subroutine read_boozer_file(file_name, B)
+    character(len=256), intent(in) :: file_name
+    real(dp), intent(out) :: B 
+    
+    integer :: file_unit
 
-            open(newunit=file_unit, file=file_name)
-            read(file_unit, *) B
-            close(file_unit)
-        end subroutine read_boozer_file
+    open(newunit=file_unit, file=file_name)
+    read(file_unit, *) B
+    close(file_unit)
+end subroutine read_boozer_file
 
 end module read_file
