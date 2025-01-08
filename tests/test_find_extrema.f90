bcbfb6394b820871e6e9511eb297e9b9ad958bc4
GeorgGrassler <georg.grassler@tugraz.at>
2025-01-08 19:38:51 +0100

    test find_global_extrema

---
+++ tests/test_find_extrema.f90
@@ -0,0 +1,33 @@
+program test_find_extrema
+use, intrinsic :: iso_fortran_env, only: dp => real64
+use find_extrema, only: find_global_extrema
+
+implicit none
+
+real(dp) :: pi = 3.14159
+real(dp) :: tol = 1e-9
+
+call test_find_global_extrema()
+
+
+contains
+
+subroutine test_find_global_extrema
+    real(dp) :: extrema(2), expected_extrema(2)
+    real(dp) :: interval(2)
+
+    interval = [0.0_dp, 2.0_dp*pi]
+    expected_extrema = (/-1.0_dp, 1.0_dp/)
+    extrema = find_global_extrema(test_func, interval)
+
+    if(any(abs(extrema/expected_extrema - 1) > tol)) STOP
+end subroutine test_find_global_extrema
+
+subroutine test_func(x, value)
+    real(dp), dimension(:), intent(in) :: x
+    real(dp), dimension(:), intent(out) :: value
+
+    value = sin(x)
+end subroutine test_func
+
+end program test_find_extrema
