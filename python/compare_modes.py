716dee6b6e9ab72e7a2347ae146db52395eb2b6d
GeorgGrassler <georg.grassler@tugraz.at>
2025-01-15 17:02:39 +0100

    splitt-off symmetric modes

--- python/compare_modes.py
+++ python/compare_modes.py
@@ -1,39 +1,6 @@
 # %%
 import matplotlib.pyplot as plt
-import numpy as np
-import copy
-
-
-class Modes:
-    def init(self):
-        self.rho_tor = np.array([])
-        self.coefs = np.array([[]])
-        self.m = np.array([[]])
-        self.n = np.array([[]])
-        self.m_max = 0
-        self.n_max = 0
-        self.get_mode_idx = lambda m, n: 0
-
-
-def read_modes_bc(bc_filename: str, get_mode_idx):
-    from libneo import BoozerFile
-
-    rmnc = Modes()
-    bc_file = BoozerFile(bc_filename)
-    rmnc.rho_tor = np.sqrt(np.array(bc_file.s))
-    rmnc.m = np.array(bc_file.m)
-    rmnc.n = np.array(bc_file.n)
-    rmnc.m_max = np.max(rmnc.m[0])
-    rmnc.n_max = np.max(rmnc.n[0])
-    rmnc.get_mode_idx = lambda m, n: get_mode_idx(m, n, n_max=rmnc.n_max)
-    zmns = copy.deepcopy(rmnc)
-    vmns = copy.deepcopy(rmnc)
-    bmnc = copy.deepcopy(rmnc)
-    rmnc.coefs = np.array(bc_file.rmnc)
-    zmns.coefs = np.array(bc_file.zmns)
-    vmns.coefs = np.array(bc_file.vmns)
-    bmnc.coefs = np.array(bc_file.bmnc)
-    return rmnc, zmns, vmns, bmnc
+from rabe.boozer_modes import read_modes_bc
 
 
 def compare_coefs(modes_1, modes_2, ms, ns):
---
+++ python/src/rabe/__init__.py
@@ -0,0 +1,1 @@
+from .boozer_modes import read_modes_bc, split_off_symmetric_modes
