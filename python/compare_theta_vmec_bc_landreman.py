a459b1dc7a8983885de2af355af64d8647fe2944
GeorgGrassler <georg.grassler@tugraz.at>
2025-01-24 14:07:15 +0100

    compare vmec and bc file directions

--- python/compare_theta_vmec_bc_landreman.py
+++ python/compare_theta_vmec_bc_landreman.py
@@ -9,6 +9,7 @@ if __name__ == "__main__":
 
     vmec_file = "output/wout_LandremanPaul2021_QH_reactorScale_lowres_reference.nc"
     vmec = Vmec(vmec_file)
+    print("For Landreman VMEC")
     get_vmec_theta_orientation(vmec)
 
     par_bc_file = "output/quasi_symmetric.bc"
@@ -16,4 +17,5 @@ if __name__ == "__main__":
         (2 * n_max + 1) * (m - 1) + (n_max + n) + (n_max + 1)
     )
     rmnc, zmns, _, _ = read_modes_bc(par_bc_file, get_mode_idx=get_mode_idx_par)
+    print("For Landreman bc file")
     get_bc_theta_orientation(rmnc, zmns)
