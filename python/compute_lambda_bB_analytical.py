9c13948487f27c1db2cdcf465df642680e98fff5
GeorgGrassler <georg.grassler@tugraz.at>
2025-03-25 13:53:22 +0100

    add shaing-callen modul

--- python/compute_lambda_bB_analytical.py
+++ python/compute_lambda_bB_analytical.py
@@ -1,119 +1,72 @@
 # %%
 import os
 import numpy as np
-from scipy.integrate import quad
+
 import h5py
 
 from simsopt.mhd.vmec import Vmec
 from simsopt.mhd.boozer import Boozer
 from simsopt.mhd.bootstrap import RedlGeomBoozer
 
-from rabe.fourier_series import FourierSeries, evaluate
-
-
-def get_bmn_splines(bc_filename):
-    from libneo import BoozerFile
-    from scipy.interpolate import interp1d
-
-    bc_file = BoozerFile(bc_filename)
-    stor = np.array(bc_file.s)
-    m = bc_file.m[0]
-    n = bc_file.n[0]
-
-    bmnc_spl = interp1d(stor, np.array(bc_file.bmnc), axis=0)
-    bmns_spl = interp1d(stor, np.array(bc_file.bmns), axis=0)
-    return bmnc_spl, bmns_spl, m, n
-
-
-def get_Bphi_spline(bc_filename):
-    from libneo import BoozerFile
-    from scipy.interpolate import interp1d
-
-    c_cgs = 3e10
-    bc_file = BoozerFile(bc_filename)
-    stor = np.array(bc_file.s)
-    Jpol_SI = np.array(bc_file.Jpol_divided_by_nper) * np.array(bc_file.nper)
-    Jpol_cgs = Jpol_SI * 3e9
-    Bphi_cgs = -2 * Jpol_cgs / c_cgs
-
-    Bphi_spl = interp1d(stor, Bphi_cgs, axis=0)
-    return Bphi_spl
+from rabe.shaing_callen import shaing_callen_bootstrap
 
 
 vmec_file = os.path.join("output/wout_axisymmetric.nc")
-neo2_file = os.path.join("output/axisymmetric.out")
+neo2_file = os.path.join("output/axisymmetric_collisionality_scan.out")
 bc_filename = os.path.join("output/axisymmetric.bc")
-bmnc_spl, bmns_spl, m, n = get_bmn_splines(bc_filename)
-Bphi_spl = get_Bphi_spline(bc_filename)
 
-surfaces = np.linspace(0.013, 0.05, 2)
+
+s_tor_vmec = [0.01, 0.02]
 helicity_n = 0
 eps = 1e-8
 
 vmec = Vmec(vmec_file)
 boozer = Boozer(vmec, mpol=16, ntor=16)
-geom = RedlGeomBoozer(boozer, surfaces, helicity_n)
+geom = RedlGeomBoozer(boozer, s_tor_vmec, helicity_n)
 
 with h5py.File(neo2_file, "r") as h5_file:
-    boozer_s = h5_file["boozer_s"][()]
+    s_tor_neo2 = h5_file["boozer_s"][()]
+    kappa = h5_file["conl_over_mfp"][()]
     avnabs = h5_file["avnabpsi"][()]
     lambda_bB_neo2output = h5_file["alambda_bb"][()]
 
 geom_data = geom()
 psi_SI = geom_data.psi_edge
+avnabpsi_cgs = avnabs[0] * psi_SI
 
-stors = []
-lambda_bB_analytic = []
-lambda_bB_neo2 = []
-for stor in boozer_s:
-    if any(np.abs(surfaces - stor) < eps):
-        idx_neo2 = np.where(np.abs(boozer_s - stor) < eps)[0][0]
+iota = geom_data.iota[0]
+b_0 = geom_data.Bmax[0]
 
-        avnabpsi_cgs = avnabs[idx_neo2] * psi_SI * 1e8
+lambda_bB_analytic = shaing_callen_bootstrap(
+    bc_filename, s_tor_neo2[0], iota, b_0, avnabpsi_cgs
+)
 
-        idx_redl = np.where(np.abs(surfaces - stor) < eps)[0][0]
-
-        iota = geom_data.iota[idx_redl]
-        B_0 = geom_data.Bmax[idx_redl]
-        bmnc = bmnc_spl(stor).tolist()
-        bmns = bmns_spl(stor).tolist()
-        fourier_b = FourierSeries(m, n, bmnc, bmns)
-
-        def integrand(x):
-            def f(theta):
-                B = evaluate(fourier_b, theta, phi=0)
-                return np.sqrt(1 - x * B / B_0) / (B / B_0) ** 2
-
-            theta_average, _ = quad(f, 0, 2 * np.pi)
-            theta_average = theta_average / (2 * np.pi)
-            return x / theta_average
-
-        Bphi_cgs = Bphi_spl(stor)
-        integral, _ = quad(integrand, 0, 1)
-        lambda_bB = -(3 / 4 * integral - 1) * Bphi_cgs / (avnabpsi_cgs * iota)
-
-        stors.append(stor)
-        lambda_bB_analytic.append(lambda_bB)
-        lambda_bB_neo2.append(lambda_bB_neo2output[idx_neo2])
 # %%
 import matplotlib.pyplot as plt
 
 plt.figure()
-plt.plot(stors, lambda_bB_analytic, "xr", label="analytic")
-plt.plot(stors, lambda_bB_neo2, "ob", label="NEO-2")
-plt.xlabel("stor")
+plt.plot(
+    -kappa,
+    -lambda_bB_analytic * np.ones_like(lambda_bB_neo2output),
+    "xr",
+    label="-analytic",
+)
+plt.plot(-kappa, lambda_bB_neo2output, "ob", label="NEO-2")
+plt.xlabel("kappa")
 plt.ylabel("lambda_bB")
+plt.xscale("log")
 plt.legend()
 plt.show()
 
 plt.figure()
 plt.plot(
-    stors,
-    np.array(lambda_bB_analytic) / np.array(lambda_bB_neo2),
+    -kappa,
+    np.array(lambda_bB_analytic) / np.array(lambda_bB_neo2output),
     "or",
     label="analytic/neo2",
 )
-plt.xlabel("stor")
+plt.xlabel("kappa")
 plt.ylabel("ratio")
+plt.xscale("log")
 plt.legend()
 plt.show()
---
+++ python/src/rabe/shaing_callen.py
@@ -0,0 +1,66 @@
+import numpy as np
+from rabe.fourier_series import FourierSeries, evaluate
+
+
+def shaing_callen_bootstrap(
+    bc_filename: str,
+    stor: np.double,
+    iota: np.double,
+    b_0: np.double,
+    avnabpsi: np.double,
+):
+    """
+    all input quantities in SI units
+    """
+    from scipy.integrate import quad
+
+    bmnc_spl, bmns_spl, m, n = get_bmn_splines(bc_filename)
+    Bphi_spl = get_Bphi_spline(bc_filename)
+    bmnc = bmnc_spl(stor).tolist()
+    bmns = bmns_spl(stor).tolist()
+    fourier_b = FourierSeries(m, n, bmnc, bmns)
+
+    def integrand(x):
+        def f(theta):
+            b = evaluate(fourier_b, theta, phi=0)
+            return np.sqrt(1 - x * b / b_0) / (b / b_0) ** 2
+
+        theta_average, _ = quad(f, 0, 2 * np.pi)
+        theta_average = theta_average / (2 * np.pi)
+        return x / theta_average
+
+    Bphi = Bphi_spl(stor)
+    integral, _ = quad(integrand, 0, 1)
+    avnabpsi_cgs = avnabpsi * 1e8
+    lambda_bB_analytic = -(3 / 4 * integral - 1) * Bphi / (avnabpsi_cgs * iota)
+
+    return lambda_bB_analytic
+
+
+def get_bmn_splines(bc_filename):
+    from libneo import BoozerFile
+    from scipy.interpolate import interp1d
+
+    bc_file = BoozerFile(bc_filename)
+    stor = np.array(bc_file.s)
+    m = bc_file.m[0]
+    n = bc_file.n[0]
+
+    bmnc_spl = interp1d(stor, np.array(bc_file.bmnc), axis=0)
+    bmns_spl = interp1d(stor, np.array(bc_file.bmns), axis=0)
+    return bmnc_spl, bmns_spl, m, n
+
+
+def get_Bphi_spline(bc_filename):
+    from libneo import BoozerFile
+    from scipy.interpolate import interp1d
+
+    c_cgs = 3e10
+    bc_file = BoozerFile(bc_filename)
+    stor = np.array(bc_file.s)
+    Jpol_SI = np.array(bc_file.Jpol_divided_by_nper) * np.array(bc_file.nper)
+    Jpol_cgs = Jpol_SI * 3e9
+    Bphi_cgs = -2 * Jpol_cgs / c_cgs
+
+    Bphi_spl = interp1d(stor, Bphi_cgs, axis=0)
+    return Bphi_spl
