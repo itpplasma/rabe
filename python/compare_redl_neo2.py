c4b6a42f201190d0dadc7ec8248255620341f464
GeorgGrassler <georg.grassler@tugraz.at>
2025-03-27 16:09:17 +0100

    put functions into module

--- python/compare_redl_neo2.py
+++ python/compare_redl_neo2.py
@@ -9,13 +9,15 @@ from simsopt.mhd.bootstrap import j_dot_B_Redl, RedlGeomBoozer
 
 import h5py
 
+from rabe.gamma_coef import get_average_gamma_coefs, convert_landreman_to_gamma_coefs
+
 vmec_file = os.path.join("output/wout_axisymmetric.nc")
 
 neo2_file = os.path.join("output/axisymmetric.out")
 
 with h5py.File(neo2_file, "r") as h5_file:
     boozer_s = h5_file["boozer_s"][()]
-    avnabpsi = h5_file["avnabpsi"][()]
+    ds_dr = h5_file["avnabpsi"][()]
     gamma_out = h5_file["gamma_out"][()]
 
 
@@ -32,24 +34,39 @@ geom = RedlGeomBoozer(boozer, boozer_s, helicity_n)
 jdotB, details = j_dot_B_Redl(ne, Te, Ti, Zeff, geom=geom, plot=False)
 
 geom_data = geom()
-psi_SI = geom_data.psi_edge
+L31 = details.L31
+L32 = details.L32
+G = geom_data.G
+I = geom_data.I
+iota = geom_data.iota
+dpsi_dr = geom_data.psi_edge * ds_dr
 
-stors = []
-gamma31_redl = []
-gamma32_redl = []
-gamma31_neo2 = []
-gamma31_std_neo2 = []
-gamma32_neo2 = []
-gamma32_std_neo2 = []
+gamma31_redl = np.zeros_like(boozer_s)
+gamma32_redl = np.zeros_like(boozer_s)
+gamma31_neo2 = np.zeros_like(boozer_s)
+gamma31_std_neo2 = np.zeros_like(boozer_s)
+gamma32_neo2 = np.zeros_like(boozer_s)
+gamma32_std_neo2 = np.zeros_like(boozer_s)
+
+for idx in range(boozer_s):
+    (
+        gamma31_neo2[idx],
+        gamma31_std_neo2[idx],
+        gamma32_neo2[idx],
+        gamma32_std_neo2[idx],
+    ) = get_average_gamma_coefs(gamma_out[idx])
+    gamma31_redl[idx], gamma32_redl[idx] = convert_landreman_to_gamma_coefs(
+        L31[idx], L32[idx], G[idx], I[idx], iota[idx], helicity_n, dpsi_dr[idx]
+    )
 
 
 # %%
 import matplotlib.pyplot as plt
 
 plt.figure()
-plt.plot(stors, -np.array(gamma31_redl), "xr", label="Redl, -gamma31")
+plt.plot(boozer_s, -np.array(gamma31_redl), "xr", label="Redl, -gamma31")
 plt.errorbar(
-    stors,
+    boozer_s,
     gamma31_neo2,
     gamma31_std_neo2,
     fmt="o",
@@ -58,9 +75,9 @@ plt.errorbar(
     ecolor="red",
     label="Neo2, gamma31",
 )
-plt.plot(stors, -np.array(gamma32_redl), "xb", label="Redl, -gamma32")
+plt.plot(boozer_s, -np.array(gamma32_redl), "xb", label="Redl, -gamma32")
 plt.errorbar(
-    stors,
+    boozer_s,
     gamma32_neo2,
     gamma32_std_neo2,
     fmt="o",
@@ -76,8 +93,12 @@ plt.legend(loc=0)
 plt.show()
 
 plt.figure()
-plt.plot(stors, np.array(gamma31_redl) / np.array(gamma31_neo2), "or", label="gamma31")
-plt.plot(stors, np.array(gamma32_redl) / np.array(gamma32_neo2), "ob", label="gamma32")
+plt.plot(
+    boozer_s, np.array(gamma31_redl) / np.array(gamma31_neo2), "or", label="gamma31"
+)
+plt.plot(
+    boozer_s, np.array(gamma32_redl) / np.array(gamma32_neo2), "ob", label="gamma32"
+)
 plt.xlabel("stor [1]")
 plt.ylabel("ratio [1]")
 plt.title("comparison dimensional bootstrap coefs")
--- python/src/rabe/gamma_coef.py
+++ python/src/rabe/gamma_coef.py
@@ -2,29 +2,25 @@ import numpy as np
 
 
 def convert_landreman_to_gamma_coefs(
-    G: float, I: float, iota: float, L31: float, L32: float, helicity_n: int
+    L31: float,
+    L32: float,
+    G: float,
+    I: float,
+    iota: float,
+    helicity_n: int,
+    dpsi_dr: float,
 ):
-    idx_neo2 = np.where(boozer_s == stor)[0][0]
 
-    av = (gamma_out[idx_neo2, 2, 0] + gamma_out[idx_neo2, 0, 2]) / 2
-    std = np.sqrt(
-        (av - gamma_out[idx_neo2, 2, 0]) ** 2 + (av - gamma_out[idx_neo2, 0, 2]) ** 2
-    )
-    gamma31_neo2.append(av)
-    gamma31_std_neo2.append(std)
-
-    av = (gamma_out[idx_neo2, 2, 1] + gamma_out[idx_neo2, 1, 2]) / 2
-    std = np.sqrt(
-        (av - gamma_out[idx_neo2, 2, 1]) ** 2 + (av - gamma_out[idx_neo2, 1, 2]) ** 2
-    )
-    gamma32_neo2.append(av)
-    gamma32_std_neo2.append(std)
-
-    dr_ds_cgs = 1 / avnabpsi[idx_neo2]
-    dr_ds_SI = dr_ds_cgs * 1e-2
-    dr_dpsi_SI = dr_ds_SI / psi_SI
-
-    conversion_fac = (G + helicity_n * I) / (iota - helicity_n) * 1 / 2 * dr_dpsi_SI
-
+    conversion_fac = (G + helicity_n * I) / (iota - helicity_n) * 1 / 2 / dpsi_dr
     gamma31_redl = conversion_fac * L31
     gamma32_redl = conversion_fac * (L32 + 5 / 2 * L31)
+    return gamma31_redl, gamma32_redl
+
+
+def get_average_gamma_coefs(gamma):
+    gamma31 = (gamma[2, 0] + gamma[0, 2]) / 2  # Onsager Symmetry
+    gamma31_std = np.sqrt((gamma31 - gamma[2, 0]) ** 2 + (gamma31 - gamma[0, 2]) ** 2)
+
+    gamma32 = (gamma[2, 1] + gamma[1, 2]) / 2  # Onsager Symmetry
+    gamma32_std = np.sqrt((gamma32 - gamma[2, 1]) ** 2 + (gamma32 - gamma[1, 2]) ** 2)
+    return gamma31, gamma31_std, gamma32, gamma32_std
