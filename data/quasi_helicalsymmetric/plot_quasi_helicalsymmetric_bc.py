a6ea20fce8f30caa9e6f5755792fe5eb4a9d9196
GeorgGrassler <georg.grassler@tugraz.at>
2025-04-03 16:09:17 +0200

    move quasi-helicalsymmetric case to data

--- data/quasi_helicalsymmetric/plot_quasi_helicalsymmetric_bc.py
+++ data/quasi_helicalsymmetric/plot_quasi_helicalsymmetric_bc.py
@@ -4,13 +4,12 @@ if __name__ == "__main__":
     from rabe.boozer_modes import read_modes_bc
     from rabe.coordinate_orientations import get_bc_theta_orientation
 
-    par_bc_file = "python/output/quasi_symmetric.bc"
+    nfp = 4
+    par_bc_file = "output/quasi_helicalsymmetric.bc"
     get_mode_idx_par = lambda m, n, n_max: (
         (2 * n_max + 1) * (m - 1) + (n_max + n) + (n_max + 1)
     )
     rmnc, zmns, vmns, bmnc = read_modes_bc(par_bc_file, get_mode_idx=get_mode_idx_par)
-    print("For Landreman bc file")
-    get_bc_theta_orientation(rmnc, zmns)
 
     from rabe.boozer_modes import (
         get_xyz_surface,
@@ -20,7 +19,7 @@ if __name__ == "__main__":
     from mpl_toolkits.mplot3d import Axes3D
     import matplotlib.pyplot as plt
 
-    x, y, z, B = get_xyz_surface(rmnc, zmns, vmns, bmnc, 4)
+    x, y, z, B = get_xyz_surface(rmnc, zmns, vmns, bmnc, nfp)
     fig = plt.figure(figsize=(8, 6))
     ax = fig.add_subplot(111, projection="3d")
     norm = plt.Normalize(B.min(), B.max())
@@ -34,19 +33,19 @@ if __name__ == "__main__":
     mappable.set_array(B)
     fig.colorbar(mappable, ax=ax, shrink=0.5, aspect=10)
 
-    theta, phi_boozer, B = get_theta_phi_surface(bmnc, 4)
+    theta, phi_boozer, B = get_theta_phi_surface(bmnc, nfp)
     fig = plt.figure(figsize=(8, 6))
     ax = fig.add_subplot(111)
     norm = plt.Normalize(B.min(), B.max())
     colors = plt.cm.viridis(norm(B))
-    surface = ax.contour(phi_boozer, theta, B)
+    surface = ax.contour(phi_boozer, theta, B, levels=20)
     ax.set_ylabel(r"$\theta_\mathrm{B}$")
     ax.set_xlabel(r"$\varphi_\mathrm{B}$")
     mappable = plt.cm.ScalarMappable(cmap="viridis", norm=norm)
     mappable.set_array(B)
     fig.colorbar(mappable, ax=ax, shrink=0.5, aspect=10)
 
-    phi_boozer, R, z = get_axis_projection(rmnc, zmns, nfp=4)
+    phi_boozer, R, z = get_axis_projection(rmnc, zmns, nfp=nfp)
     fig = plt.figure(figsize=(8, 6))
     ax = fig.add_subplot(111)
     ax.plot(R, z, "r.", label="approx. magnetic axis")
