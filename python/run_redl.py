01a44799f1262dca5c13fc36ed69682da03cb2c0
GeorgGrassler <georg.grassler@tugraz.at>
2025-04-16 13:03:28 +0200

    set to Landreman figure 1 config

--- python/run_redl.py
+++ python/run_redl.py
@@ -8,13 +8,13 @@ from simsopt.mhd.profiles import ProfilePolynomial
 from simsopt.mhd.bootstrap import j_dot_B_Redl, RedlGeomBoozer
 
 filename = os.path.join(
-    "python/output/wout_LandremanPaul2021_QH_reactorScale_lowres_reference.nc"
+    "output/wout_LandremanPaul2021_QH_reactorScale_lowres_reference.nc"
 )
 
-ne = ProfilePolynomial(4.0e20 * np.array([1]))
+ne = ProfilePolynomial(4.13e20 * np.array([1]))
 Te = ProfilePolynomial(12.0e3 * np.array([1, -1]))
-Ti = ProfilePolynomial(6.0e3 * np.array([1]))
-surfaces = np.linspace(0.3, 0.7, 3)
+Ti = Te
+surfaces = np.linspace(0.1, 0.9, 30)
 Zeff = 1
 helicity_n = -1
 
@@ -31,11 +31,12 @@ plt.title("J dot B")
 plt.legend(loc=0)
 plt.show()
 
-for idx in range(len(surfaces)):
-    print("--------------------------------")
-    print("s = ", surfaces[idx])
-    print("L31 = ", details.L31[idx])
-    print("L32 = ", details.L32[idx])
-    print("L34 = ", details.L34[idx])
-    print("alpha = ", details.alpha[idx])
-    print("<j dot B> = ", jdotB[idx])
+if False:
+    for idx in range(len(surfaces)):
+        print("--------------------------------")
+        print("s = ", surfaces[idx])
+        print("L31 = ", details.L31[idx])
+        print("L32 = ", details.L32[idx])
+        print("L34 = ", details.L34[idx])
+        print("alpha = ", details.alpha[idx])
+        print("<j dot B> = ", jdotB[idx])
