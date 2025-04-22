50acb913bf82d9f736d21334b634c303570d18cc
GeorgGrassler <georg.grassler@tugraz.at>
2025-04-22 17:37:10 +0200

    move to seperate module

--- app/main.f90
+++ app/main.f90
@@ -2,6 +2,7 @@ program rabe
     use constants, only: dp, pi
     use neo_field, only: neo_field_t
     use find_extrema, only: find_local_maxima
+    use fieldline_labels, only: guess_alpha_at_minimum
 
     implicit none
 
@@ -26,41 +27,6 @@ program rabe
     print *, alpha_at_min
 
 contains
-    subroutine guess_alpha_at_minimum(field, alpha_at_min, M)
-        use field_base, only: field_t
-        use find_extrema, only: find_local_minima
-
-        class(field_t), intent(in) :: field
-        real(dp), intent(in) :: M
-        real(dp), intent(out) :: alpha_at_min
-
-        real(dp), dimension(2), parameter :: interval = (/0.0_dp, 4.0_dp*pi/)
-        real(dp) :: location(1)
-
-        call find_local_minima(estimate_B_mod_of_alpha, interval, location)
-        alpha_at_min = mod(location(1), 2*pi)
-
-    end subroutine guess_alpha_at_minimum
-
-    subroutine estimate_B_mod_of_alpha(alpha, B_mod)
-        real(dp), dimension(:), intent(in) :: alpha
-        real(dp), dimension(:), intent(out) :: B_mod
-
-        real(dp), dimension(size(alpha, 1)) :: phi, theta
-        real(dp) :: dummy, dummy_sqrtg, dummy_dB_dx(3)
-        integer :: idx
-
-        ! If f(theta,phi) approx f(alpha = M*theta - N*phi) one can estiamte
-        ! f(alpha) by choosing 1 specific theta-phi combination for that
-        ! alpha value e.g. phi=0 and theta=alpha/M
-        phi = 0.0_dp
-        theta = alpha/M
-
-        do idx = 1, size(alpha, 1)
-            call field%compute_B_sqrtg_dB_dx(theta(idx), phi(idx), B_mod(idx), &
-                                             dummy_sqrtg, dummy_dB_dx)
-        end do
-    end subroutine estimate_B_mod_of_alpha
 
     subroutine field_along_fieldline(phi, B_mod)
         real(dp), dimension(:), intent(in) :: phi
---
+++ src/fieldline_labels.f90
@@ -0,0 +1,42 @@
+module fieldline_labels
+    use constants, only: dp, pi
+    implicit none
+contains
+    subroutine guess_alpha_at_minimum(field, alpha_at_min, M)
+        use field_base, only: field_t
+        use find_extrema, only: find_local_minima
+
+        class(field_t), intent(in) :: field
+        real(dp), intent(in) :: M
+        real(dp), intent(out) :: alpha_at_min
+
+        real(dp), dimension(2), parameter :: interval = (/0.0_dp, 4.0_dp*pi/)
+        real(dp) :: location(1)
+
+        call find_local_minima(estimate_B_mod_of_alpha, interval, location)
+        alpha_at_min = mod(location(1), 2*pi)
+
+    contains
+
+        subroutine estimate_B_mod_of_alpha(alpha, B_mod)
+            real(dp), dimension(:), intent(in) :: alpha
+            real(dp), dimension(:), intent(out) :: B_mod
+
+            real(dp), dimension(size(alpha, 1)) :: phi, theta
+            real(dp) :: dummy, dummy_sqrtg, dummy_dB_dx(3)
+            integer :: idx
+
+            ! If f(theta,phi) approx f(alpha = M*theta - N*phi) one can estiamte
+            ! f(alpha) by choosing 1 specific theta-phi combination for that
+            ! alpha value e.g. phi=0 and theta=alpha/M
+            phi = 0.0_dp
+            theta = alpha/M
+
+            do idx = 1, size(alpha, 1)
+                call field%compute_B_sqrtg_dB_dx(theta(idx), phi(idx), B_mod(idx), &
+                                                 dummy_sqrtg, dummy_dB_dx)
+            end do
+        end subroutine estimate_B_mod_of_alpha
+
+    end subroutine guess_alpha_at_minimum
+end module fieldline_labels
