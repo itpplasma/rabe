4c8300eeaa3ef5fbe8dc8d072e827a5530d3e832
GeorgGrassler <georg.grassler@tugraz.at>
2025-04-24 17:58:29 +0200

    check if is_same

--- src/utils.f90
+++ src/utils.f90
@@ -17,4 +17,26 @@ contains
             x(i) = a + (i - 1)*dx
         end do
     end subroutine linspace
+
+    function is_same(array_1, array_2, reltol_in, abstol_in)
+        real(dp), dimension(:), intent(in) :: array_1, array_2
+        real(dp), intent(in), optional :: reltol_in, abstol_in
+        logical :: is_same
+
+        real(dp) :: reltol, abstol
+
+        if (present(reltol_in)) then
+            reltol = reltol_in
+        else
+            reltol = 0.0_dp
+        end if
+        if (present(abstol_in)) then
+            abstol = abstol_in
+        else
+            abstol = reltol
+        end if
+
+        is_same = any(abs(array_1 - array_2) > reltol*array_1 + abstol)
+    end function is_same
+
 end module utils
