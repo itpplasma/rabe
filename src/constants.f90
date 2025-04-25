a585e29d3c1640b9c8d6ac4b2de13bb8eeeb5807
GeorgGrassler <georg.grassler@tugraz.at>
2025-04-25 17:54:51 +0200

    update constants

--- src/constants.f90
+++ src/constants.f90
@@ -2,5 +2,5 @@ module constants
     implicit none
 
     integer, parameter :: dp = kind(1.0d0)
-    real(dp), parameter :: pi = 3.14159
+    real(dp), parameter :: pi = 3.1415926535897932
 end module constants
