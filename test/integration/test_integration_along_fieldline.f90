e914a3fd4b5bf422cfc7f0bb8ac86b51bf6fe423
GeorgGrassler <georg.grassler@tugraz.at>
2025-04-25 14:09:20 +0200

    test integration along fieldline

--- test/integration/test_integration_along_fieldline.f90
+++ test/integration/test_integration_along_fieldline.f90
@@ -1,58 +1,58 @@
 program test_fieldline
     use constants, only: dp, pi
     use utils, only: is_same
-    use mock_field, only: mock_field_t
 
     implicit none
 
-    real(dp), parameter :: theta_mode = 2.0_dp, phi_mode = -4.0_dp
-    real(dp), parameter :: B_0 = 1.0_dp, B_amplitude = -0.5_dp
-    type(mock_field_t) :: field
-
-    call field%mock_field_init(theta_mode, phi_mode, B_0, B_amplitude)
-    call test_get_fieldlines()
+    call test_integration_along_fieldline()
 
 contains
 
-    subroutine test_get_fieldlines()
+    subroutine test_integration_along_fieldline()
+        use mock_field, only: mock_field_t
         use fieldline_mod, only: fieldline_t
         use fieldline_mod, only: set_fieldline_phi_0_to_mode_minimum
         use fieldline_mod, only: find_maxima_along_fieldline
-        use utils, only: linspace
 
         real(dp), parameter :: reltol = 1e-2
-        real(dp), parameter :: stor = 0.5_dp
-        integer, parameter :: n_fieldlines = 10, n_maxima = 2
 
-        real(dp), dimension(n_fieldlines) :: theta_0
-        type(fieldline_t), dimension(n_fieldlines) :: fieldlines
+        real(dp), parameter :: theta_mode = 1.0_dp, phi_mode = -4.0_dp
+        real(dp), parameter :: B_0 = 1.0_dp, B_amplitude = 0.5_dp
+        type(mock_field_t) :: field
 
-        integer :: current
+        real(dp), parameter :: theta_0 = -3.0_dp*pi
+        real(dp), parameter :: iota = -3.0_dp
+        integer, parameter :: n_maxima = 2
+        type(fieldline_t), dimension(1) :: fieldline
+
         real(dp) :: interval(2)
-        real(dp) :: phi_max(n_maxima)
+        real(dp) :: integral, found_integral
 
-        call linspace(0.0_dp, 2.0_dp*pi, n_fieldlines, theta_0)
-        fieldlines(:)%theta_0 = theta_0(:)
-        fieldlines(:)%iota = -1.0_dp
+        call field%mock_field_init(theta_mode, phi_mode, B_0, B_amplitude)
+
+        fieldline(1)%theta_0 = theta_0
+        fieldline(1)%iota = iota
+        allocate (fieldline(1)%phi_max(n_maxima))
 
         call set_fieldline_phi_0_to_mode_minimum(field, theta_mode, phi_mode, &
-                                                 fieldlines)
+                                                 fieldline)
 
-        do current = 1, n_fieldlines
-            allocate (fieldlines(current)%phi_max(n_maxima))
-            interval = (/0.0_dp, 2*pi/) + fieldlines(current)%phi_0
-            call find_maxima_along_fieldline(field, fieldlines(current), &
-                                             interval, fieldlines(current)%phi_max)
-            phi_max = (/0.5*pi, 1.5*pi/) + fieldlines(current)%phi_0
-            if (is_same(phi_max, fieldlines(current)%phi_max, reltol)) then
-                print *, "-------------------------------------------------------------"
-                print *, "test_get_fieldlines failed: phi_max"
-                print *, "found: ", fieldlines(current)%phi_max
-                print *, "expected: ", phi_max
-                error stop
-            end if
-        end do
+        print *, "phi_0: ", fieldline(1)%phi_0
+        interval = (/0.0_dp, 4*pi/) + fieldline(1)%phi_0
+        call find_maxima_along_fieldline(field, fieldline(1), interval, &
+                                         fieldline(1)%phi_max)
 
-    end subroutine test_get_fieldlines
+        found_integral = 0.0_dp
+
+        integral = 2.0_dp*pi*B_0
+        if (is_same(integral, found_integral, reltol)) then
+            print *, "-------------------------------------------------------------"
+            print *, "test_integration_along_fieldline failed: integral"
+            print *, "found: ", found_integral
+            print *, "expected: ", integral
+            error stop
+        end if
+
+    end subroutine test_integration_along_fieldline
 
 end program test_fieldline
