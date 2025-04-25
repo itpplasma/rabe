eda36b2e5de55d7204924ab03043fd0a869ad550
GeorgGrassler <georg.grassler@tugraz.at>
2025-04-25 11:37:09 +0200

    update main

--- app/main.f90
+++ app/main.f90
@@ -1,14 +1,39 @@
 program rabe
     use constants, only: dp, pi
+    use utils, only: linspace
     use neo_field, only: neo_field_t
-    use fieldline_mod, only: guess_alpha_over_M_at_minimum, find_maxima_along_fieldline
+    use fieldline_mod, only: fieldline_t
+    use fieldline_mod, only: set_fieldline_phi_0_to_mode_minimum
+    use fieldline_mod, only: find_maxima_along_fieldline
 
     implicit none
 
+    real(dp), parameter :: stor = 0.5_dp
+    integer, parameter :: n_fieldlines = 10, n_maxima = 2
+    real(dp), parameter :: theta_mode = 1.0_dp, phi_mode = -4.0_dp
     character(len=*), parameter :: bc_filename = "test/integration/input/"// &
                                    "quasi_helical.bc"
-    type(neo_field_t) :: field
 
-    call field%neo_field_init(bc_filename, stor=0.5_dp)
+    type(neo_field_t) :: field
+    type(fieldline_t), dimension(n_fieldlines) :: fieldlines
+    real(dp), dimension(n_fieldlines) :: theta_0
+
+    integer :: current
+    real(dp) :: interval(2)
+
+    call field%neo_field_init(bc_filename, stor)
+
+    call linspace(0.0_dp, 2.0_dp*pi, n_fieldlines, theta_0)
+    fieldlines(:)%theta_0 = theta_0(:)
+    fieldlines(:)%iota = field%iota
+
+    call set_fieldline_phi_0_to_mode_minimum(field, theta_mode, phi_mode, &
+                                             fieldlines)
+    do current = 1, n_fieldlines
+        allocate (fieldlines(current)%phi_max(n_maxima))
+        interval = (/0.0_dp, 2*pi/) + fieldlines(current)%phi_0
+        call find_maxima_along_fieldline(field, fieldlines(current), &
+                                         interval, fieldlines(current)%phi_max)
+    end do
 
 end program rabe
