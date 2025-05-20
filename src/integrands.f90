3bb40aa4f1ca2deb1bd9758a18fb858c6fcee420
GeorgGrassler <georg.grassler@tugraz.at>
2025-05-20 09:15:10 +0200

    extend integrands

--- src/integrands.f90
+++ src/integrands.f90
@@ -6,45 +6,56 @@ module integrands
 
 contains
 
-    function B_mod_squared(field, theta, phi)
-        class(field_t), intent(in) :: field
-        real(dp), intent(in) :: theta, phi
-        real(dp) :: B_mod_squared
-
-        real(dp) :: B_mod
-
-        call field%compute_B_mod(theta, phi, B_mod)
-        B_mod_squared = B_mod**2
-    end function B_mod_squared
-
-    function lambda_squared(field, theta, phi, eta)
-        class(field_t), intent(in) :: field
-        real(dp), intent(in) :: theta, phi, eta
-        real(dp) :: lambda_squared
-
-        real(dp) :: B_mod
-
-        call field%compute_B_mod(theta, phi, B_mod)
-        lambda_squared = 1 - B_mod*eta
-
-        if (lambda_squared .lt. 0.0_dp) then
-            print *, "Square of pitch parameter is negative!"
-            print *, "B_mod: ", B_mod
-            print *, "eta: ", eta
-            error stop
-        end if
-
-    end function lambda_squared
-
     function radial_drift_velocity(field, theta, phi, eta)
         class(field_t), intent(in) :: field
         real(dp), intent(in) :: theta, phi, eta
         real(dp) :: radial_drift_velocity
 
-        real(dp) :: B_mod, lambda_sqared, dB_dtheta_0
+        real(dp) :: B, dB_dx(3), lambda_squared, dB_dtheta_0
 
-        error stop "radial_drift_veloctiy not yet implemented!"
+        call field%compute_B_and_dB_dx(theta, phi, B, dB_dx)
+        lambda_squared = pitchparameter_squared(B, eta)
 
+        dB_dtheta_0 = dB_dx(2)
+
+        radial_drift_velocity = sqrt(lambda_squared)/B**3*(3 + lambda_squared) &
+                                *dB_dtheta_0
     end function radial_drift_velocity
+
+    function lambda_over_B_squared(field, theta, phi, eta)
+        class(field_t), intent(in) :: field
+        real(dp), intent(in) :: theta, phi, eta
+        real(dp) :: lambda_over_B_squared
+
+        real(dp) :: B
+
+        call field%compute_B_mod(theta, phi, B)
+        lambda_over_B_squared = sqrt(pitchparameter_squared(B, eta))/B**2
+    end function lambda_over_B_squared
+
+    function B_squared(field, theta, phi)
+        class(field_t), intent(in) :: field
+        real(dp), intent(in) :: theta, phi
+        real(dp) :: B_squared
+
+        real(dp) :: B
+
+        call field%compute_B_mod(theta, phi, B)
+        B_squared = B**2
+    end function B_squared
+
+    function pitchparameter_squared(B, eta)
+        real(dp), intent(in) :: B, eta
+        real(dp) :: pitchparameter_squared
+
+        pitchparameter_squared = 1 - B*eta
+
+        if (pitchparameter_squared .lt. 0.0_dp) then
+            print *, "Square of pitch parameter (1 - B*eta) is negative!"
+            print *, "B: ", B
+            print *, "eta: ", eta
+            error stop
+        end if
+    end function pitchparameter_squared
 
 end module integrands
