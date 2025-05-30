0d6c746be18e3c38a1a39ec693d493bfc10a5a44
GeorgGrassler <georg.grassler@tugraz.at>
2025-05-30 13:28:38 +0200

    add contour

--- test/integration/plot/myplot_module.f90
+++ test/integration/plot/myplot_module.f90
@@ -9,25 +9,60 @@ module myplot_module
     contains
         procedure :: initialize
         procedure :: add_plot
+        procedure :: add_contour
         procedure :: show
     end type myplot
 
 contains
 
-    subroutine initialize(self)
+    subroutine initialize(self, xlabel, ylabel)
         class(myplot), intent(out) :: self
 
-        call self%plt%initialize()
+        character(len=*), intent(in), optional :: xlabel, ylabel
+
+        call self%plt%initialize(grid=.true., &
+                                 xlabel=xlabel, &
+                                 ylabel=ylabel, &
+                                 raw_strings=.true.)
     end subroutine initialize
 
-    subroutine add_plot(self, x, f, label, linestyle)
+    subroutine add_plot(self, x, f, label, linestyle, linewidth, markersize)
         class(myplot), intent(inout) :: self
         real(dp), dimension(:), intent(in) :: x
         real(dp), dimension(size(x)), intent(in) :: f
         character(len=*), intent(in) :: label, linestyle
 
-        call self%plt%add_plot(x, f, label, linestyle)
+        integer, intent(in), optional :: linewidth
+        integer, intent(in), optional :: markersize
+        call self%plt%add_plot(x, f, label, linestyle, &
+                               linewidth=linewidth, &
+                               markersize=markersize)
     end subroutine add_plot
+
+    subroutine add_contour(self, x, y, f, levels, colorbar, filled)
+        use utils, only: linspace
+        class(myplot), intent(inout) :: self
+        real(dp), dimension(:), intent(in) :: x, y
+        real(dp), dimension(size(x), size(y)), intent(in) :: f
+        integer, optional :: levels
+        logical, optional :: colorbar
+        logical, optional :: filled
+
+        real(dp), dimension(:), allocatable :: levels_pyplot
+
+        if (present(levels)) then
+            allocate (levels_pyplot(levels))
+            call linspace(minval(f), maxval(f), levels, levels_pyplot)
+            call self%plt%add_contour(x, y, f, linestyle="-", &
+                                      levels=levels_pyplot, &
+                                      colorbar=colorbar, &
+                                      filled=filled)
+        else
+            call self%plt%add_contour(x, y, f, linestyle="-", &
+                                      colorbar=colorbar, &
+                                      filled=filled)
+        end if
+    end subroutine add_contour
 
     subroutine show(self)
         class(myplot), intent(inout) :: self
