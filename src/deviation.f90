49e934bc580d6c40a695e28bdfd9333dc952f1ff
GeorgGrassler <georg.grassler@tugraz.at>
2025-06-05 20:48:36 +0200

    calc deviation

--- src/deviation.f90
+++ src/deviation.f90
@@ -13,6 +13,69 @@ module deviation
 
 contains
 
+    subroutine calc_deviation(fieldlines, field, deviation_A, deviation_B)
+        use fieldline_integrals, only: fieldline_modes_t
+        use fieldline_integrals, only: allocate_modes
+        use fieldline_integrals, only: fourier_transform_over_label
+        use misc, only: S_A, S_B
+
+        type(fieldline_t), dimension(:), intent(in) :: fieldlines
+        class(field_t), intent(in) :: field
+        real(dp), intent(out) :: deviation_A, deviation_B
+
+        real(dp), parameter :: tol = 1e-12
+
+        type(fieldline_modes_t) :: modes
+        real(dp) :: iota_p, eta_b, I_ref_psi_edge
+        type(surface_average_t) :: average
+        real(dp) :: symmetric_remainder
+        real(dp) :: B_squared_sqrtg
+
+        call fourier_transform_over_label(fieldlines, modes)
+        call calc_surface_averages(fieldlines, average)
+
+        iota_p = fieldlines(1)%iota_p
+        eta_b = fieldlines(1)%eta_b
+        I_ref_psi_edge = fieldlines(1)%I_ref*get_B_squared_sqrtg_psi_edge(field)
+
+        deviation_A = sum(modes%radial_drift%sin_coeffs* &
+                          modes%delta_aspect_ratio%cos_coeffs* &
+                          S_A(iota_p*modes%delta_aspect_ratio%mode_numbers))
+
+        symmetric_remainder = sum(modes%radial_drift%cos_coeffs* &
+                                  modes%delta_aspect_ratio%sin_coeffs* &
+                                  S_A(iota_p*modes%delta_aspect_ratio%mode_numbers))
+
+        if (abs(symmetric_remainder/deviation_A) > tol) then
+            print *, "warning: non-vanishing symmetric part of deviation A: "
+            print *, "symmetric: ", symmetric_remainder
+            print *, "antisymmetric: ", deviation_A
+            print *, "ratio: ", symmetric_remainder/deviation_A
+        end if
+
+        deviation_A = deviation_A*average%B_squared/average%lambda_b* &
+                      sqrt(eta_b*I_ref_psi_edge)/average%normalization
+
+        deviation_B = sum(modes%radial_drift%sin_coeffs* &
+                          modes%delta_eta%cos_coeffs* &
+                          S_B(iota_p*modes%delta_eta%mode_numbers))
+
+        symmetric_remainder = sum(modes%radial_drift%cos_coeffs* &
+                                  modes%delta_aspect_ratio%sin_coeffs* &
+                                  S_B(iota_p*modes%delta_aspect_ratio%mode_numbers))
+
+        if (abs(symmetric_remainder/deviation_B) > tol) then
+            print *, "warning: non-vanishing symmetric part of deviation B: "
+            print *, "symmetric: ", symmetric_remainder
+            print *, "antisymmetric: ", deviation_B
+            print *, "ratio: ", symmetric_remainder/deviation_B
+        end if
+
+        deviation_B = deviation_B*average%B_squared/average%lambda_b*0.5_dp/ &
+                      average%normalization
+
+    end subroutine calc_deviation
+
     subroutine calc_surface_averages(fieldlines, surface_average)
         type(fieldline_t), dimension(:), intent(in) :: fieldlines
         type(surface_average_t), intent(out) :: surface_average
@@ -25,5 +88,18 @@ contains
         surface_average%lambda_b = sum(fieldlines%integral_lambda_b_over_B_squared)/ &
                                    surface_average%normalization
     end subroutine calc_surface_averages
+
+    function get_B_squared_sqrtg_psi_edge(field) result(B_squared_sqrtg_psi_edge)
+        use field_base, only: field_t
+
+        class(field_t), intent(in) :: field
+
+        real(dp) :: B_squared_sqrtg_psi_edge
+        real(dp), parameter :: theta = 0.0_dp, phi = 0.0_dp
+        real(dp) :: sqrtg, B_mod, dummy_dB_dx(3)
+
+        call field%compute_B_sqrtg_dB_dx(theta, phi, B_mod, sqrtg, dummy_dB_dx)
+        B_squared_sqrtg_psi_edge = abs(B_mod**2.0_dp*sqrtg)
+    end function get_B_squared_sqrtg_psi_edge
 
 end module deviation
