8ceb4771b0b6f9a2f3e5a4e49fafde8303ef6a75
GeorgGrassler <georg.grassler@tugraz.at>
2025-06-06 09:57:35 +0200

    do not check against analytic

--- test/integration/test_against_neo2_anti_sigma.f90
+++ test/integration/test_against_neo2_anti_sigma.f90
@@ -13,7 +13,7 @@ program test_against_neo2_anti_sigma
     integer, parameter :: n_cases = 4
     real(dp) :: stor(n_cases), theta(n_cases), phi(n_cases)
     real(dp) :: bmod
-    real(dp) :: bmod_analytic(n_cases), bmod_neo2(n_cases)
+    real(dp) :: bmod_neo2(n_cases)
     real(dp), parameter :: B_min = 0.875_dp, B_max = 1.05_dp
     integer :: case
 
@@ -29,11 +29,6 @@ program test_against_neo2_anti_sigma
     theta = (/0.0_dp, 0.25_dp*pi, 2.0_dp*pi, 0.0_dp/)
     phi = (/0.0_dp, 0.15_dp*pi, 0.0_dp, -0.1_dp*pi/)
 
-    bmod_analytic = (/B_min, &
-                      B_max, &
-                      B_min, &
-                      B_max/)
-
     bmod_neo2 = (/0.86660052969026313_dp, &
                   1.0488269770770471_dp, &
                   0.87920244657041735_dp, &
@@ -43,14 +38,6 @@ program test_against_neo2_anti_sigma
     do case = 1, n_cases
         call field%neo_change_stor(stor(case))
         call field%compute_B_mod(theta(case), phi(case), bmod)
-        if (abs(bmod/bmod_analytic(case) - 1) > retol) then
-            print *, "-------------------------------------------------------------"
-            print *, "test_against_neo2_field failed: B analytic"
-            print *, "B: ", bmod
-            print *, "analytic: ", bmod_analytic(case)
-            print *, "relative error: ", 1.0_dp - bmod/bmod_analytic(case)
-            test_failed = .true.
-        end if
         if (abs(bmod/bmod_neo2(case) - 1) > retol) then
             print *, "-------------------------------------------------------------"
             print *, "test_against_neo2_field failed: B NEO-2"
