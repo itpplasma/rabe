1ba61ea7a49620dd56dd7cca74c74fcf647289e5
GeorgGrassler <georg.grassler@tugraz.at>
2025-06-12 15:36:33 +0200

    import plotting routines

--- test/integration/plot_deviation_helical_anti_sigma.f90
+++ test/integration/plot_deviation_helical_anti_sigma.f90
@@ -6,6 +6,10 @@ program plot_deviation_helical_anti_sigma
     use fieldline_mod, only: fieldline_t
     use make_fieldline, only: make_flock_of_fieldlines
     use deviation, only: calc_deviation
+
+    use plot_quantities, only: plot_fieldlines_over_field
+    use plot_quantities, only: plot_delta_eta
+    use plot_quantities, only: plot_delta_A
     use plot_quantities, only: plot_deviation
 
     implicit none
@@ -48,9 +52,8 @@ program plot_deviation_helical_anti_sigma
                                   N_tor, &
                                   phi_tol)
 
-    call plot_fieldlines_over_field(fieldlines, field)
+    call plot_fieldlines_over_field(fieldlines, field, N_tor)
     call plot_delta_eta(fieldlines)
-    call plot_I(fieldlines)
     call plot_delta_A(fieldlines, delta_A_1)
 
     call calc_deviation(fieldlines, field, deviation_A, deviation_B)
@@ -60,118 +63,5 @@ program plot_deviation_helical_anti_sigma
     off_factor_B = deviation_B*0.5*R*pi*dr_dpsi
 
     call plot_deviation(off_factor_A, off_factor_B)
-
-contains
-
-    subroutine plot_delta_eta(fieldlines)
-        type(fieldline_t), dimension(:), intent(in) :: fieldlines
-
-        call plt%initialize(xlabel="$\vartheta_{mid}$", &
-                            ylabel="$\Delta \eta$")
-
-        call plt%add_plot(fieldlines%theta_0, &
-                          fieldlines%delta_eta, &
-                          label="\Delta \eta", &
-                          linestyle="-")
-        call plt%show()
-    end subroutine plot_delta_eta
-
-    subroutine plot_delta_A(fieldlines, delta_A_1_analytic)
-        type(fieldline_t), dimension(:), intent(in) :: fieldlines
-        real(dp), intent(in) :: delta_A_1_analytic
-
-        call plt%initialize(xlabel="$\vartheta_{mid}$", &
-                            ylabel="$\Delta A$", &
-                            legend=.true.)
-
-        call plt%add_plot(fieldlines%theta_0, &
-                          fieldlines%delta_aspect_ratio, &
-                          label="$\Delta A$", &
-                          linestyle="-")
-        call plt%add_plot(fieldlines%theta_0, &
-                          delta_A_1_analytic*cos(fieldlines%theta_0), &
-                          label="$\Delta A$ analytic", &
-                          linestyle="-")
-        call plt%show()
-    end subroutine plot_delta_A
-
-    subroutine plot_I(fieldlines)
-        type(fieldline_t), dimension(:), intent(in) :: fieldlines
-
-        integer :: n_fieldlines
-        real(dp) :: I_mean
-
-        call plt%initialize(xlabel="$\vartheta_{mid}$", &
-                            ylabel="$I$")
-
-        n_fieldlines = size(fieldlines)
-        I_mean = sum(fieldlines%integral_lambda_b_over_B_squared)/n_fieldlines
-        call plt%add_plot(fieldlines%theta_0, &
-                          fieldlines%integral_lambda_b_over_B_squared/I_mean - 1.0_dp, &
-                          label="$I", &
-                          linestyle="-")
-        call plt%show()
-    end subroutine plot_I
-
-    subroutine plot_fieldlines_over_field(fieldlines, field)
-        use myplot_module, only: myplot
-        use field_base, only: field_t
-
-        type(fieldline_t), dimension(:), intent(in) :: fieldlines
-        class(field_t), intent(in) :: field
-
-        type(myplot) :: plt
-        integer :: current
-        integer :: n_fieldlines
-        real(dp), dimension(2) :: phi_max, theta_max
-        real(dp), dimension(2) :: phi_limits
-        integer, parameter :: n_points = 300
-        real(dp), dimension(n_points) :: phi, theta
-        real(dp), dimension(n_points, n_points) :: phi_mesh, theta_mesh
-        real(dp), dimension(n_points, n_points) :: B_mesh
-        integer :: theta_idx, phi_idx
-        character(len=100) :: label
-
-        n_fieldlines = size(fieldlines)
-
-        call plt%initialize(xlabel="$\varphi$", ylabel="$\vartheta$")
-
-        phi_limits = +(/-1.5_dp*pi/N_tor + fieldlines(1)%phi_0, &
-                        +1.5_dp*pi/N_tor + fieldlines(n_fieldlines)%phi_0/)
-        do current = 1, size(fieldlines)
-            phi_max = fieldlines(current)%phi_max
-            call linspace(phi_max(1), &
-                          phi_max(2), &
-                          n_points, &
-                          phi)
-            theta = fieldlines(current)%get_theta(phi)
-            write (label, '(F0.1)') fieldlines(current)%theta_0
-            call plt%add_plot(phi, theta, &
-                              label=label, &
-                              linestyle="k.", &
-                              linewidth=1)
-            theta_max = fieldlines(current)%get_theta(phi_max)
-            call plt%add_plot(phi_max, theta_max, &
-                              label=label, &
-                              linestyle="ro", &
-                              markersize=5)
-        end do
-
-        call linspace(phi_limits(1), phi_limits(2), n_points, phi)
-        call linspace(0.0_dp, 2.0_dp*pi, n_points, theta)
-        do theta_idx = 1, n_points
-            do phi_idx = 1, n_points
-                call field%compute_B_mod(theta(theta_idx), &
-                                         phi(phi_idx), &
-                                         B_mesh(theta_idx, phi_idx))
-            end do
-        end do
-        call plt%add_contour(phi, theta, transpose(B_mesh), &
-                             levels=20, &
-                             colorbar=.true., &
-                             filled=.true.)
-        call plt%show()
-
-    end subroutine plot_fieldlines_over_field
 
 end program plot_deviation_helical_anti_sigma
