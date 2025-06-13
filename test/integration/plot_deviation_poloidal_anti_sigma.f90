62409f35b5593da8ffdb051e4fe1281bdce0746a
GeorgGrassler <georg.grassler@tugraz.at>
2025-06-13 18:55:02 +0200

    check against analytical formula in poloidal anti-sigma

--- test/integration/plot_deviation_poloidal_anti_sigma.f90
+++ test/integration/plot_deviation_poloidal_anti_sigma.f90
@@ -15,29 +15,31 @@ program plot_deviation_poloidal_anti_sigma
 
     implicit none
 
-    real(dp), parameter :: M_pol = 2.0_dp, N_tor = 10.0_dp
-    character(len=*), parameter :: bc_filename = "input/helical_anti_small_aspect.bc"
-    real(dp), parameter :: psi_edge = abs(-0.28274_dp)/(2.0_dp*pi)/160000.0_dp !Tm^2
-    real(dp), parameter :: R = 8.0_dp
-    real(dp), parameter :: J_pol_over_N_tor = -4.0_dp*1e6
+    real(dp), parameter :: M_pol = 0.0_dp, N_tor = 1.0_dp
+    character(len=*), parameter :: bc_filename = "input/poloidal_anti.bc"
+    real(dp), parameter :: psi_edge = abs(-0.00785398_dp)/(2.0_dp*pi) !Tm^2
+    real(dp), parameter :: R = 1.00_dp
+    real(dp), parameter :: J_pol_over_N_tor = -5.0_dp*1e6
     real(dp), parameter :: I_tor = 0.0_dp
 
-    real(dp), parameter :: stor = 0.9999_dp
-    real(dp), parameter :: ds_dr = 0.0661777_dp*100.0_dp*400.0_dp !1/m
+    real(dp), parameter :: stor = 0.25781_dp
+    real(dp), parameter :: ds_dr = 0.387524_dp*100.0_dp !1/m
     real(dp), parameter :: dr_dpsi = 1.0_dp/(ds_dr*psi_edge)
 
-    real(dp), parameter :: B_0 = 1.0_dp, eps_0 = -0.005, eps_1 = -0.00009375_dp
-    real(dp), parameter :: delta_B_1 = -0.1_dp
+    real(dp), parameter :: B_0 = 1.0_dp, eps_0 = 0.125, eps_1 = 0.05_dp
+    real(dp), parameter :: delta_B_1 = 0.0_dp*1e-4
     real(dp), parameter :: eps_ratio = eps_1/abs(eps_0)
-    real(dp), parameter :: delta_A_1 = 0.25_dp*eps_ratio*(1.0_dp - 6.0_dp*eps_0)
+    real(dp), parameter :: delta_A_1 = 0.25_dp*eps_ratio*(1.0_dp + 6.0_dp*abs(eps_0))
+    real(dp), parameter :: B_max = B_0*(1.0_dp + eps_0) + abs(delta_B_1)
+    real(dp), parameter :: delta_eta_1 = delta_B_1/B_max**2.0_dp
     type(neo_field_t) :: field
 
-    real(dp), parameter :: phi_tol = 1e-6
+    real(dp), parameter :: phi_tol = 4e-6
     integer, parameter :: n_fieldlines = 20
 
     real(dp), dimension(n_fieldlines) :: theta_0
     real(dp), dimension(n_fieldlines + 1) :: temp
-    real(dp), parameter :: iota = 0.47_dp
+    real(dp), parameter :: iota = 0.1618_dp
     type(fieldline_t), dimension(n_fieldlines) :: fieldlines
 
     real(dp) :: deviation_A, deviation_B
@@ -65,7 +67,7 @@ program plot_deviation_poloidal_anti_sigma
 
     if (should_plot_others) then
         call plot_fieldlines_over_field(fieldlines, field, N_tor)
-        call plot_delta_eta(fieldlines)
+        call plot_delta_eta(fieldlines, delta_eta_1, fieldlines(1)%iota_p)
         call plot_delta_A(fieldlines, delta_A_1)
     end if
 
@@ -77,7 +79,8 @@ program plot_deviation_poloidal_anti_sigma
 
     off_factor_A_analytic = -dr_dpsi*sqrt(2.0_dp*pi)*R*B_0*abs(eps_1)**2.0_dp &
                             *N_tor*S_A(iota_p)/(2.0_dp*abs(eps_0))**0.75_dp &
-                            /(N_tor - iota*M_pol)**1.5_dp*0.5_dp*(1.0_dp - 6.0_dp*eps_0)
+                            /(N_tor - iota*M_pol)**1.5_dp*0.5_dp &
+                            *(1.0_dp + 6.0_dp*abs(eps_0))
 
     off_factor_B_analytic = eps_1*dr_dpsi*R*pi*delta_B_1*S_B(iota_p)*0.25_dp
 
