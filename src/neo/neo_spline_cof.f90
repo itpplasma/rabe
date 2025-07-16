c0e4d86cb833c03d9b3c03482bfc5e9457e96cec
Christopher Albert <albert@tugraz.at>
2025-07-16 17:36:51 +0200

    Fix index of spline coefficient

--- src/neo/neo_spline_cof.f90
+++ src/neo/neo_spline_cof.f90
@@ -46,8 +46,8 @@ SUBROUTINE splinecof3_fast(x, y, a, b, c, d)
   b(1) = r(1)/h(1) - h(1)/3d0*cs(1)
   b(2:n-2) = r(2:n-2)/h(2:n-2)-h(2:n-2)/3d0*(cs(2:n-2) + 2d0*cs(1:n-3))
   b(n-1)   = r(n-1)/h(n-1)-h(n-1)/3d0*(2d0*cs(n-2))
-  c(4)     = 0
-  c(2:n-1)    = cs
+  c(1)     = 0d0
+  c(2:n-1) = cs
   d(1)     = 1d0/(3d0*h(1))*cs(1)
   d(2:n-2) = 1d0/(3d0*h(2:n-2))*(cs(2:n-2)-cs(1:n-3))
   d(n-1)   = 1d0/(3d0*h(n-1))*(-cs(n-2))
