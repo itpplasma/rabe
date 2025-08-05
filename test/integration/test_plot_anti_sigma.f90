73eeda4d97c1f2e4f4e4b3c88d21ac084b20be3f
GeorgGrassler <georg.grassler@tugraz.at>
2025-08-05 21:35:15 +0200

    use module function

--- test/integration/test_plot_anti_sigma.f90
+++ test/integration/test_plot_anti_sigma.f90
@@ -7,6 +7,8 @@ program test_plot_anti_sigma
     use fieldline_mod, only: fieldline_t
     use make_fieldline, only: make_flock_of_fieldlines
 
+    use plot_quantities, only: plot_fieldlines_over_field
+
     implicit none
 
     real(dp), parameter :: M_pol = 0.0_dp, N_tor = 1.0_dp
@@ -39,66 +41,6 @@ program test_plot_anti_sigma
                                   N_tor, &
                                   phi_tol)
 
-    call plot_fieldlines_over_field(fieldlines, perturbed_field)
-
-contains
-
-    subroutine plot_fieldlines_over_field(fieldlines, field)
-        use myplot_module, only: myplot
-        use field_base, only: field_t
-
-        type(fieldline_t), dimension(:), intent(in) :: fieldlines
-        class(field_t), intent(in) :: field
-
-        type(myplot) :: plt
-        integer :: current
-        real(dp), dimension(2) :: phi_max, theta_max
-        real(dp), dimension(2) :: phi_limits
-        integer, parameter :: n_points = 100
-        real(dp), dimension(n_points) :: phi, theta
-        real(dp), dimension(n_points, n_points) :: phi_mesh, theta_mesh
-        real(dp), dimension(n_points, n_points) :: B_mesh
-        integer :: theta_idx, phi_idx
-        character(len=100) :: label
-
-        call plt%initialize(xlabel="$\varphi$", ylabel="$\vartheta$")
-
-        phi_limits = fieldlines(1)%phi_0 + (/-1.5_dp*pi, +1.5_dp*pi/)
-        do current = 1, size(fieldlines)
-            phi_max = fieldlines(current)%phi_max
-            call linspace(phi_max(1), &
-                          phi_max(2), &
-                          n_points, &
-                          phi)
-            theta = fieldlines(current)%get_theta(phi)
-            write (label, '(F0.1)') fieldlines(current)%theta_0
-            call plt%add_plot(phi, theta, &
-                              label=label, &
-                              linestyle="k.", &
-                              linewidth=1)
-            theta_max = fieldlines(current)%get_theta(phi_max)
-            call plt%add_plot(phi_max, theta_max, &
-                              label=label, &
-                              linestyle="ro", &
-                              markersize=5)
-        end do
-
-        call linspace(phi_limits(1), phi_limits(2), n_points, phi)
-        call linspace(0.0_dp, 2.0_dp*pi, n_points, theta)
-        do theta_idx = 1, n_points
-            do phi_idx = 1, n_points
-                call field%compute_B_mod(theta(theta_idx), &
-                                         phi(phi_idx), &
-                                         B_mesh(theta_idx, phi_idx))
-            end do
-        end do
-        B_mesh = transpose(B_mesh)
-        call plt%add_contour(phi, theta, B_mesh, &
-                             levels=20, &
-                             colorbar=.true., &
-                             filled=.true.)
-        call plt%show()
-
-    end subroutine plot_fieldlines_over_field
+    call plot_fieldlines_over_field(fieldlines, perturbed_field, N_tor)
 
 end program test_plot_anti_sigma
