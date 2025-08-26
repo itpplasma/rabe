a2d70f5accd85eaea30b95f63d767f409e379352
GeorgGrassler <georg.grassler@tugraz.at>
2025-08-26 09:26:46 +0200

    refactor: remove redundancy

--- test/integration/test_set_fieldline_labels.f90
+++ test/integration/test_set_fieldline_labels.f90
@@ -81,16 +81,14 @@ program test_set_fieldline_labels
         end do
         call make_flock_of_fieldlines(fieldlines, xi_0, iota, field, M_pol, N_tor, &
                                       nfp, phi_tol=phi_tol)
-        call calc_deviation(fieldlines, dummy_A, dummy_B)
         if (should_plot) then
-            call make_flock_of_fieldlines(fieldlines, xi_0, iota, field, M_pol, N_tor, &
-                                          nfp, phi_tol=phi_tol)
             call plot_fieldlines_over_field_chi_xi(fieldlines, field, M_pol, N_tor, &
                                                    nfp)
             call plot_maxima_over_label(fieldlines)
             call plot_fieldlines_over_field(fieldlines, field, N_tor)
             call plot_delta_A(fieldlines)
         end if
+        call calc_deviation(fieldlines, dummy_A, dummy_B)
     end do
 
     if (test_failed) error stop
