40ff34e416ed7605bfbe1596a8269c94e1ca2878
GeorgGrassler <georg.grassler@tugraz.at>
2026-01-02 16:06:53 +0100

    test: total remainder between

--- test/integration/test_shaing_callen_remainder_scaling.f90
+++ test/integration/test_shaing_callen_remainder_scaling.f90
@@ -7,12 +7,14 @@ program test_shaing_callen_remainder_scaling
     use utils, only: linspace, not_same
 
     use shaing_callen_remainder, only: get_non_omnigenous_remainder_pitch
+    use shaing_callen_mod, only: calc_trapped_fraction
+    use shaing_callen_remainder, only: calc_trapped_fraction_prime
 
     use plot_quantities, only: plot_non_omnigenous_remainder
 
     implicit none
 
-    real(dp), parameter :: phi_tol = 7e-7
+    real(dp), parameter :: phi_tol = 7e-6
     integer, parameter :: n_fieldlines = 50
 
     real(dp), dimension(n_fieldlines) :: xi_0
@@ -21,7 +23,7 @@ program test_shaing_callen_remainder_scaling
 
     real(dp), parameter :: nfp = 4.0_dp, iota = 1.24_dp
     real(dp), parameter :: M_pol = -1.0_dp, N_tor = nfp
-    real(dp), parameter :: B_0 = 1.0_dp, eps_0 = -0.3_dp
+    real(dp), parameter :: B_0 = 1.0_dp, eps_0 = -0.15_dp
     type(mock_field_t) :: qs_field
 
     real(dp), parameter :: M_pol_pert = 1.0_dp, N_tor_pert = 0.0_dp
@@ -31,8 +33,11 @@ program test_shaing_callen_remainder_scaling
     integer, parameter :: n_eta = 400
     integer :: this
     integer, parameter :: n_eps = 8
-    real(dp), dimension(n_eps) :: eps, remainder
-    real(dp), parameter :: abstol_factor = 2.46e-1
+    real(dp), dimension(n_eps) :: eps, remainder_pitch, remainder
+    real(dp), parameter :: estimated_scaling_pitch = abs(1.42*M_pol/nfp)* &
+                           (1.0_dp - 2.0_dp*abs(eps_0))
+    real(dp), parameter :: estimated_scaling = (1.0_dp - 2.0_dp*abs(eps_0)) &
+                           /sqrt(abs(eps_0))
     real(dp) :: abstol
 
     logical, parameter :: should_plot = .false.
@@ -44,7 +49,7 @@ program test_shaing_callen_remainder_scaling
     call linspace(0.0_dp, 2.0_dp*pi, n_fieldlines + 1, temp)
     xi_0 = temp(1:n_fieldlines)
 
-    call linspace(1.0_dp, real(n_eps, kind=dp), n_eps, eps)
+    call linspace(2.0_dp, real(n_eps, kind=dp), n_eps, eps)
     eps = 10.0_dp**(-eps)
 
     do this = 1, n_eps
@@ -59,13 +64,31 @@ program test_shaing_callen_remainder_scaling
                                       nfp, &
                                       phi_tol)
 
-        remainder(this) = get_non_omnigenous_remainder_pitch(field, &
-                                                             fieldlines, &
-                                                             n_eta)
-        abstol = abstol_factor*sqrt(abs(eps(this)))
+        remainder_pitch(this) = get_non_omnigenous_remainder_pitch(field, &
+                                                                   fieldlines, &
+                                                                   n_eta)
+        abstol = estimated_scaling_pitch*sqrt(abs(eps(this)))
+        if (not_same(remainder_pitch(this), 0.0_dp, &
+                     reltol_in=0.0_dp, abstol_in=abstol)) then
+            print *, "-------------------------------------------------------------"
+            print *, "test_get_non_omnigenous_remainder_scaling failed: ", &
+                "pitch boundary term"
+            print *, "omnigenity violation = ", eps(this)
+            print *, "abs remainder = ", abs(remainder_pitch(this))
+            print *, "expected remainder = ", abstol
+            test_failed = .true.
+        end if
+
+        remainder(this) = calc_trapped_fraction(field, fieldlines, n_eta) * M_pol/(M_pol*iota - N_tor)
+        remainder(this) = 1.0_dp - calc_trapped_fraction_prime(field, fieldlines, n_eta)/remainder(this)
+
+        ! We increase tolerance of ~20% as for the analytic estimate only the
+        ! contribution of the above pitch boundary term remainder was considered
+        ! and other linear terms where ignored
+        abstol = estimated_scaling*sqrt(abs(eps(this)))*1.2_dp
         if (not_same(remainder(this), 0.0_dp, reltol_in=0.0_dp, abstol_in=abstol)) then
             print *, "-------------------------------------------------------------"
-            print *, "test_get_non_omnigenous_remainder_scaling failed:"
+            print *, "test_get_non_omnigenous_remainder_scaling failed: 1 - (M\iota - N)f_t'/(M f_t)"
             print *, "omnigenity violation = ", eps(this)
             print *, "abs remainder = ", abs(remainder(this))
             print *, "expected remainder = ", abstol
@@ -74,7 +97,14 @@ program test_shaing_callen_remainder_scaling
     end do
 
     if (should_plot) then
-        call plot_non_omnigenous_remainder(eps, remainder)
+        call plot_non_omnigenous_remainder(eps, &
+                                           remainder_pitch, &
+                                           "pitch boundary term", &
+                                           estimated_scaling_pitch)
+        call plot_non_omnigenous_remainder(eps, &
+                                           remainder, &
+                                         "$1 - \frac{M\iota - N}{M}\frac{f_t'}{f_t}$", &
+                                           estimated_scaling)
     end if
 
     if (test_failed) error stop
