84b8d835dc9ea27a6d1f04b6fbd8ed5197b461d3
GeorgGrassler <georg.grassler@tugraz.at>
2026-03-27 17:27:42 +0100

    check: input/output
    of finite boundary layer and surface average calc

--- src/coefficients.f90
+++ src/coefficients.f90
@@ -1,6 +1,8 @@
 module coefficients
-    use constants, only: dp, pi
+    use constants, only: dp, pi, eps
     use fieldline_mod, only: fieldline_t
+    use, intrinsic :: ieee_arithmetic, only: ieee_is_nan
+
     implicit none
 
 contains
@@ -22,21 +24,52 @@ contains
         real(dp) :: helical_factor, covariant_factor
         type(surface_average_t) :: average
 
+        if (R < eps) then
+            print *, "error: major radius R must be positive."
+            print *, "R =", R
+            error stop
+        end if
+        if (ieee_is_nan(dr_dAtheta)) then
+            print *, "error: dr_dAtheta is NaN."
+            error stop
+        end if
+        if (ieee_is_nan(B_theta_covariant)) then
+            print *, "error: B_theta_covariant is NaN."
+            error stop
+        end if
+        if (ieee_is_nan(B_phi_covariant)) then
+            print *, "error: B_phi_covariant is NaN."
+            error stop
+        end if
+
         call calc_surface_averages(fieldlines, average)
 
         M_pol = fieldlines(1)%M_pol
         N_tor = fieldlines(1)%N_tor
         iota = fieldlines(1)%iota
         I_ref = fieldlines(1)%I_ref
+        eta_b = fieldlines(1)%eta_b
+
+        covariant_factor = (B_phi_covariant + B_theta_covariant*iota)
+        if (covariant_factor < eps) then
+            print *, "error: covariant factor must be positive."
+            print *, "B_phi_covariant =", B_phi_covariant
+            print *, "B_theta_covariant =", B_theta_covariant
+            print *, "iota =", iota
+            error stop
+        end if
 
         helical_factor = (B_phi_covariant*M_pol + B_theta_covariant*N_tor) &
                          /(M_pol*iota - N_tor)
-        covariant_factor = (B_phi_covariant*M_pol + B_theta_covariant*N_tor)
 
         I_ref_hat = I_ref/eta_b*covariant_factor/R
         Lambda_finite = sqrt(I_ref_hat)*average%B_squared*eta_b**2/average%lambda_b
         Lambda_finite = 0.75_dp*0.855_dp/pi*sqrt(2.0_dp)*Lambda_finite
         Lambda_finite = -helical_factor*dr_dAtheta*Lambda_finite
+        if (ieee_is_nan(Lambda_finite)) then
+            print *, "error: Lambda_finite is NaN."
+            error stop
+        end if
     end function calc_finite_boundary_layer_correction
 
 end module coefficients
