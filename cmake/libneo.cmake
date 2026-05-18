a15684e2d2a109236f2c98c738253687253894f3
GeorgGrassler <georg.grassler@tugraz.at>
2026-05-18 18:20:32 +0200

    pin: libneo head commit

--- cmake/libneo.cmake
+++ cmake/libneo.cmake
@@ -9,7 +9,7 @@ else()
     FetchContent_Declare(
         libneo
         GIT_REPOSITORY https://github.com/itpplasma/libneo.git
-        GIT_TAG        main
+        GIT_TAG        752aae9c8e31136121bbd77ce439f51e47a754be
         PATCH_COMMAND  ${CMAKE_COMMAND} -E copy
                        ${CMAKE_SOURCE_DIR}/cmake/libneo_CMakeLists.txt
                        <SOURCE_DIR>/CMakeLists.txt
