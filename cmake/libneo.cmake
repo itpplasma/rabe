fc06004d9c899aeb31562415ce8fc3522e7a64c0
GeorgGrassler <100583655+GeorgGrassler@users.noreply.github.com>
2026-05-19 12:57:40 +0200

    Fetch/libneo subpackages (#68)
    * fetch: only needed src from libneo
    
    * fix: $CODE fallback
    
    using the old find_or_fetch branch
    
    * pin: libneo head commit
    
    * remove: not needed dependencies

---
+++ cmake/libneo.cmake
@@ -0,0 +1,18 @@
+include(FetchContent)
+
+if(DEFINED ENV{CODE} AND EXISTS "$ENV{CODE}/libneo")
+    message(STATUS "Using libneo in $ENV{CODE}/libneo")
+    add_subdirectory("$ENV{CODE}/libneo"
+                     "${CMAKE_CURRENT_BINARY_DIR}/libneo"
+                     EXCLUDE_FROM_ALL)
+else()
+    FetchContent_Declare(
+        libneo
+        GIT_REPOSITORY https://github.com/itpplasma/libneo.git
+        GIT_TAG        752aae9c8e31136121bbd77ce439f51e47a754be
+        PATCH_COMMAND  ${CMAKE_COMMAND} -E copy
+                       ${CMAKE_SOURCE_DIR}/cmake/libneo_CMakeLists.txt
+                       <SOURCE_DIR>/CMakeLists.txt
+    )
+    FetchContent_MakeAvailable(libneo)
+endif()
