8dc510aa7443d2224ed634a8b4c67c1b39b64c2b
Christopher Albert <albert@tugraz.at>
2026-08-24 22:18:42 +0200

    Make golden workflow accept Fortio candidates

--- cmake/fortio.cmake
+++ cmake/fortio.cmake
@@ -1,5 +1,6 @@
 include(FetchContent)
 
+# -DFORTIO_REF=<branch|tag|SHA>: fetch this Fortio revision instead of the pin.
 set(FORTIO_REF "471256b7382afb8f641f37d833cc9172605da018" CACHE STRING
     "fortio git ref (branch, tag, or SHA) to fetch")
 
