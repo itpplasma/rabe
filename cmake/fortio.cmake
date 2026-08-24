dc98695f82e190b8cb03b3d04dc8f775fcbc3105
Christopher Albert <albert@tugraz.at>
2026-08-25 00:28:56 +0200

    build: update Fortio pin

--- cmake/fortio.cmake
+++ cmake/fortio.cmake
@@ -4,7 +4,7 @@ if(NOT TARGET fortio)
     FetchContent_Declare(
         fortio
         GIT_REPOSITORY https://github.com/lazy-fortran/fortio.git
-        GIT_TAG ce5c7257563648dc5afbdfbe5b5a91181bf06912
+        GIT_TAG 829fcde02024047f05e7a2389a01709617525e13
     )
     FetchContent_MakeAvailable(fortio)
 endif()
