367e5d0d971ec3351521b452398e797f39f013c2
Christopher Albert <albert@tugraz.at>
2026-08-25 16:51:27 +0200

    build: track Fortio lifecycle bridge

--- cmake/fortio.cmake
+++ cmake/fortio.cmake
@@ -4,7 +4,7 @@ if(NOT TARGET fortio)
     FetchContent_Declare(
         fortio
         GIT_REPOSITORY https://github.com/lazy-fortran/fortio.git
-        GIT_TAG 4af4f714eecc1430879c95dd144f6ead229158ce
+        GIT_TAG 11afd0bd1af0c99ea4e9a1c0df683dbdcc299b69
     )
     FetchContent_MakeAvailable(fortio)
 endif()
